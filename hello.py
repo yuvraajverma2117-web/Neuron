e0a61b1c94c090d1fb01c3720747708c0174d008
yuvraajverma2117-web <yuvraajverma2117@gmail.com>
2026-03-24 14:21:51 +0000

    Tue, Mar 24, 2026, 7:51 PM +05:30

---
+++ hello.py
@@ -0,0 +1,7 @@
+n= 16
+m= 17
+print("The value of", n, ' + ', m, ' is ', n+m )
+print("The value of", n, ' + ', m, ' is ', n+m )
+print("The value of", n, ' + ', m, ' is ', n+m )
+print("The value of", n, ' + ', m, ' is ', n+m )
+
